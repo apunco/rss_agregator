0440ea9a036399cd03a6c8218f4e1efaaa835aaf
apunco <puncochar.a@gmail.com>
2024-11-25 23:26:11 +0100

    added login middleware, added rss scraping, agg function

---
+++ sql/schema/009_posts.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+CREATE TABLE gator.posts (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title TEXT NOT NULL,
+    url TEXT NOT NULL,
+    description TEXT,
+    published_at TIMESTAMP NOT NULL,
+    feed_id UUID NOT NULL,
+    CONSTRAINT fk_post_feed
+    FOREIGN KEY (feed_id)
+    REFERENCES gator.feeds(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE gator.posts;
+
